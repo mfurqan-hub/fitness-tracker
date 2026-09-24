8f7834a058f2dac625881e29c3e10706878aa413
Furqan <enquiries@elevateimperial.com>
2026-09-24 09:26:12 +0500

    Expose exact serverless initialization errors in api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,12 @@
-const app = require('../server');
-
-module.exports = app;
+module.exports = async (req, res) => {
+  try {
+    const app = require('../server');
+    return app(req, res);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Vercel Lambda Initialization Error',
+      message: err.message,
+      stack: err.stack
+    });
+  }
+};
